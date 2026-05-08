cfc4ebe3cc8220e25021563809a53f2ab6aa1e6e
yakexerr <senya.evseev.05@mail.ru>
2026-05-08 23:23:17 +0300

    frontend: very important fix in profile

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -1,21 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // берем логин из localStorage
     const userLogin = localStorage.getItem('userLogin');
+    const userId = localStorage.getItem('userId') || 1; // Берем ID для "стабильного" рандома
 
     if (!userLogin) {
-        // если логина нет, значит пользователь не вошел — кидаем на страницу входа
         window.location.href = '/pages/authPage.html';
         return;
     }
 
-    // запрашиваем данные с сервера
+    // -- для аватарки
+    const avatarCount = 5; // Укажи здесь, сколько у тебя картинок в папке assets
+    const avatarIndex = Math.floor(Math.random() * avatarCount) + 1; // если каждый раз при обновлении
+    // const avatarIndex = (userId % avatarCount) + 1; // если не каждый раз
+    const avatarImg = document.getElementById('p-avatar');
+    if (avatarImg) {
+        avatarImg.src = `/assets/ava${avatarIndex}.jpeg`;
+    }
+    // --
+
     fetch(`/api/user-info?username=${userLogin}`)
         .then(res => res.json())
         .then(user => {
             if (user.error) {
                 alert("Ошибка загрузки данных");
             } else {
-                // заполняем поля на странице
                 document.getElementById('p-name').textContent = user.name;
                 document.getElementById('p-lastname').textContent = user.lastname;
                 document.getElementById('p-birthDate').textContent = user.birthDate;
